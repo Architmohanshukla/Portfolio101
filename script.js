3ef2d68b5da99bd88c0c0765abdac26e1cecc26c
Architmohanshukla <99432098+Architmohanshukla@users.noreply.github.com>
2023-12-20 18:06:28 +0530

    js update

--- script.js
+++ script.js
@@ -32,6 +32,37 @@ document.querySelectorAll(".photo").forEach((photo) => {
                closeLightbox();
            }
        });
+// Add this JavaScript to your existing script.js file
+
+function openModal(imageSrc, caption) {
+    const modal = document.querySelector(".modal");
+    const modalImage = document.getElementById("modalImage");
+
+    modalImage.src = imageSrc;
+    modalImage.alt = caption;
+
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    const modal = document.querySelector(".modal");
+    modal.style.display = 'none';
+}
+
+// Close modal if clicked outside the image
+window.addEventListener("click", (event) => {
+    const modal = document.querySelector(".modal");
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+// Keyboard navigation for the modal
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        closeModal();
+    }
+});
 
        // Keyboard navigation for the lightbox
        document.addEventListener("keydown", (event) => {
